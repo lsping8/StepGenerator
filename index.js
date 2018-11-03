db6612ef47346ab21467f8c9473c10a614dbe16d
Jason Loh <ksloh1991@gmail.com>
2018-11-03 16:08:54 +0800

    change logic

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const Gpio = require('pigpio').Gpio;
 const motor = new Gpio(18, {mode: Gpio.OUTPUT});
 
 let pulseWidth = 1000;
-let increment = 100;
+let increment = 1000;
 
 setInterval(() => {
   motor.servoWrite(pulseWidth);
 
   pulseWidth += increment;
   if (pulseWidth >= 2000) {
-    increment = -100;
+    increment = -1000;
   } else if (pulseWidth <= 1000) {
-    increment = 100;
+    increment = 1000;
   }
-}, 1000);
+}, 5000);
