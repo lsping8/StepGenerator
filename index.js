a36f09759032a50e717f4fd4fc50ee6efaaaa526
Jason Loh <ksloh1991@gmail.com>
2018-11-03 15:06:55 +0800

    added console.log in script

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function ever
 
 function blinkLED() { //function to start blinking
   if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+    console.log('change to 1');
     LED.writeSync(1); //set pin state to 1 (turn LED on)
   } else {
+    console.log('change to 0');
     LED.writeSync(0); //set pin state to 0 (turn LED off)
   }
 }
