cce0d58fe5807f7942a53aa27432d5905f0ea8d5
Jason Loh <ksloh1991@gmail.com>
2018-11-24 18:22:23 +0800

    make sure servo start at center

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const motor = new Gpio(18, {mode: Gpio.OUTPUT});
 let pulseWidth = 1000;
 let rotateClockWise = true;
 
-setInterval(() => {
-    motor.servoWrite(pulseWidth);
+motor.servoWrite(1500);
 
-    if (rotateClockWise) {
-        rotateClockWise = false;
-        pulseWidth = 1500;
-    } else {
-        rotateClockWise = true;
-        pulseWidth = 1000;
-    }
-}, 350);
+// setInterval(() => {
+//     motor.servoWrite(pulseWidth);
+
+//     if (rotateClockWise) {
+//         rotateClockWise = false;
+//         pulseWidth = 1500;
+//     } else {
+//         rotateClockWise = true;
+//         pulseWidth = 1000;
+//     }
+// }, 350);
