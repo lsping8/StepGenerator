cf0894e718b72004785e97859ca3c81e0de4e644
Jason Loh <ksloh1991@gmail.com>
2018-11-24 18:35:38 +0800

    change pulseWidth value and added pulseWidth to make servo stay center before starting

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const Gpio = require('pigpio').Gpio;
 
 const motor = new Gpio(18, {mode: Gpio.OUTPUT});
 
-let pulseWidth = 1000;
+let pulseWidth = 1200;
 let rotateClockWise = true;
 
 setInterval(() => {
     console.log('process.env.CENTER', process.env.CENTER);
-    motor.servoWrite(process.env.CENTER || 1500);
+    motor.servoWrite(process.env.CENTER || pulseWidth);
 
-    // if (rotateClockWise) {
-    //     rotateClockWise = false;
-    //     pulseWidth = 1500;
-    // } else {
-    //     rotateClockWise = true;
-    //     pulseWidth = 1000;
-    // }
+    if (rotateClockWise) {
+        rotateClockWise = false;
+        pulseWidth = 1400;
+    } else {
+        rotateClockWise = true;
+        pulseWidth = 1000;
+    }
 }, 350);
