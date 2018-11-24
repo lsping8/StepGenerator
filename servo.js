f783baf86401a796f25e2639dc47c5fc4bac7e68
Jason Loh <ksloh1991@gmail.com>
2018-11-24 19:37:37 +0800

    added code to start jonny-five with raspi-io

--- servo.js
+++ servo.js
@@ -1,5 +1,8 @@
+const Raspi = require('raspi-io');
 const five = require("johnny-five");
-const board = new five.Board();
+const board = new five.Board({
+    io: new Raspi()
+  });
 
 board.on("ready", () => {
   const servo = new five.Servo(18);
